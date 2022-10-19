9154ba540e18aa8d7cc4e857122d4bc93ede173f
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-10-19 12:33:19 +0300

    added 11 task

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+const debounce = require('lodash.debounce');
 const gallery = document.querySelector('.gallery');
 const btnMore=document.querySelector('[type="button"]')
 const axios = require('axios').default;
@@ -26,18 +27,18 @@ const MAIN_URL_WITH_KEY = 'https://pixabay.com/api/?key=30629726-597c78df0089c17
 function createElements(response) {
   gallery.insertAdjacentHTML('beforeend', response.data.hits.map(card).join(''));
    lightbox.refresh();
-      if (response.data.hits.length < 40&& response.data.hits.length !== 0) {
+      if (response.data.hits.length < 40&& response.data.hits.length !== 0||page===13) {
         btnMore.classList.replace('load-more', 'visually-hidden');
         setTimeout(() => {
       Notiflix.Notify.info("Were sorry, but you've reached the end of search results.");
-     }, 1000);
+        }, 1000);
+       
    }
 }
   
  function auditAndBuild (response) {
    if (response.data.totalHits === 0) {
      Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-     
     } else {
       Notiflix.Notify.success(`SHooray! We found ${response.data.totalHits} images.`);
       btnMore.classList.replace('visually-hidden', 'load-more');
@@ -65,15 +66,8 @@ function onSubmitClick(event) {
  
 }
 
-// window.addEventListener('scroll', () => {
-//   const documentRect = gallery.getBoundingClientRect();
-//   if (documentRect.bottom = document.documentElement.clientHeight + 150) {
-//     page += 1;
-//     getUser().then(res => moreBuild(res)).catch(err => console.log(err))
-//   }
-// })
-
 const onBtnMoreClick = () => {
+  
   page +=1;
   getUser().then(res => moreBuild(res)).catch(err => console.log(err)) 
 }
